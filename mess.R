87baa38af280ba4498f7fd34f099aee9f3cc65db
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2022-07-25 12:24:20 +0200

    udp

--- mess.R
+++ mess.R
@@ -1,6 +1,8 @@
 
-githubURL <- "https://github.com/jazznbass/supplement-social-relational-classroom-climate-2022/raw/master/data_l1.rds"
+l1 <- "https://github.com/jazznbass/supplement-social-relational-classroom-climate-2022/raw/master/data_l1.rds"
+l2 <- "https://github.com/jazznbass/supplement-social-relational-classroom-climate-2022/raw/master/data_l2.rds"
 
-dat <- readRDS(url(githubURL))
+dat_l1 <- readRDS(url(l1))
+dat_l2 <- readRDS(url(l2))
 
-
+dat_l2
