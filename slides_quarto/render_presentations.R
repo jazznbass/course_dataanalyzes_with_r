f3394d780c143981d9649e07ecf23a78628dd3e4
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2022-08-16 15:01:25 +0200

    upd quarto

---
+++ slides_quarto/render_presentations.R
@@ -0,0 +1,35 @@
+
+
+info <<-
+  list(
+  company = "University of Potsdam",
+  author = "Jürgen Wilbert",
+  date = paste0('Version: ', format(Sys.time(), '%d %B %Y'))
+)
+
+files <- dir(pattern = "*.Rmd")
+
+files <- c(
+  #"01-Introduction.Rmd",
+ #"02-Basics_1.Rmd",
+
+
+   #"02-Basics_soph_sub.Rmd"
+#  "02b-Basics_misc.Rmd"
+#  "03-Rmarkdown.Rmd",
+ #"04-Tidyverse.Rmd",
+  "04b-dplyr.Rmd"
+#  "06-ggplot2.Rmd",
+#  "05-Statistics.Rmd"
+  #"07-Regression.Rmd"
+)
+
+files <- "09-control-functions.Rmd"
+
+for(file  in files) {
+  rmarkdown::render(
+    file,
+    output_dir = "html"
+  )
+}
+
